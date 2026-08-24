c9c018f90dbd71a497f8e9b8dbe74ce76f7fbab4
f2reninj5 <64966099+f2reninj5@users.noreply.github.com>
2026-08-24 23:28:32 +0100

    Rename variable to camel case

--- src/component/logo.tsx
+++ src/component/logo.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import { cn } from "@/lib/utils";
-import transparent_default from "../asset/transparent_default.svg";
+import transparentDefault from "../asset/transparent_default.svg";
 
 export default function Logo({ className }: { className?: string }) {
   return (
     <Image
       className={cn("w-[1em] h-[1em]", className)}
-      src={transparent_default}
+      src={transparentDefault}
       alt="Durham Robotics"
     />
   );
