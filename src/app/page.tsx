a9da680fc3a95e2c168d3989b1558a5aacf8009f
f2reninj5 <64966099+f2reninj5@users.noreply.github.com>
2026-08-24 23:29:35 +0100

    Update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,27 @@
+import { faDiscord, faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import type { ReactNode } from "react";
 import Logo from "@/component/logo";
+import { cn } from "@/lib/utils";
 
 function ButtonLink({
   href,
+  className,
   children,
 }: {
   href: string;
+  className?: string;
   children?: ReactNode;
 }) {
   return (
-    <a className="bg-contrast text-background p-10 rounded-3xl" href={href}>
+    <a
+      className={cn(
+        "w-fit h-fit bg-primary text-background p-10 rounded-3xl transition duration-500 ease-out hover:scale-110",
+        className,
+      )}
+      href={href}
+      target="_blank"
+    >
       {children}
     </a>
   );
@@ -20,16 +32,25 @@ export default function Home() {
     <>
       <header className="flex flex-row items-center justify-center gap-5 p-10 text-6xl">
         <Logo className="text-8xl" />
-        <h1 className="font-bold">Durham Robotics</h1>
+        <h1 className="font-bold inline-block max-w-full">Durham Robotics</h1>
       </header>
-      <main className="flex flex-1 flex-col items-center justify-start text-5xl gap-10">
-        <ButtonLink href="https://discord.gg/NW5sPcCgfk">Discord</ButtonLink>
-        <ButtonLink href="https://instagram.com/du_robotics/">
-          Instagram
-        </ButtonLink>
-        <ButtonLink href="https://drive.google.com/drive/folders/1zgly1DJN1CDVUM2_YmNYN1t8M5ar_YZF">
-          Session Material
-        </ButtonLink>
+      <main className="flex-1 flex flex-col items-center justify-start text-5xl gap-10">
+        <div className="flex-2 flex items-center justify-center">
+          <ButtonLink
+            href="https://forms.gle/ZYESVUPDZFtXi4zy5"
+            className="bg-background text-contrast warm-glow "
+          >
+            Join the Exec
+          </ButtonLink>
+        </div>
+        <div className="flex-1 flex flex-row gap-10 p-10">
+          <ButtonLink href="https://discord.gg/NW5sPcCgfk">
+            <FontAwesomeIcon icon={faDiscord} />
+          </ButtonLink>
+          <ButtonLink href="https://instagram.com/du_robotics/">
+            <FontAwesomeIcon icon={faInstagram} />
+          </ButtonLink>
+        </div>
       </main>
     </>
   );
