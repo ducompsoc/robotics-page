07123416d919f73d472aad9599cf2fda2162cafd
f2reninj5 <64966099+f2reninj5@users.noreply.github.com>
2026-08-24 22:04:10 +0100

    Add links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,19 @@
+import type { ReactNode } from "react";
 import Logo from "@/component/logo";
+
+function ButtonLink({
+  href,
+  children,
+}: {
+  href: string;
+  children?: ReactNode;
+}) {
+  return (
+    <a className="bg-contrast text-background p-10 rounded-3xl" href={href}>
+      {children}
+    </a>
+  );
+}
 
 export default function Home() {
   return (
@@ -7,7 +22,15 @@ export default function Home() {
         <Logo className="text-8xl" />
         <h1 className="font-bold">Durham Robotics</h1>
       </header>
-      <main className="flex flex-1 flex-col items-center justify-start"></main>
+      <main className="flex flex-1 flex-col items-center justify-start text-5xl gap-10">
+        <ButtonLink href="https://discord.gg/NW5sPcCgfk">Discord</ButtonLink>
+        <ButtonLink href="https://instagram.com/du_robotics/">
+          Instagram
+        </ButtonLink>
+        <ButtonLink href="https://drive.google.com/drive/folders/1zgly1DJN1CDVUM2_YmNYN1t8M5ar_YZF">
+          Session Material
+        </ButtonLink>
+      </main>
     </>
   );
 }
